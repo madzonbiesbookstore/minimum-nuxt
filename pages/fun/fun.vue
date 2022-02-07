13b16978f1efce0eb95bbf9ca5c45430c27ed7ed
madzonbiesbookstore <madzonbiesbookstore@gmail.com>
2022-02-07 22:06:29 +0000

    template edit

--- pages/fun/fun.vue
+++ pages/fun/fun.vue
@@ -1,3 +1,18 @@
 <template>
-    <p>fun,fun</p>
+  <div>
+    <h1>Hello {{name}}!</h1> <!-- nameの表示 …（1） -->
+    <input v-model="name"><!-- nameの入力 …（2）-->
+    {{counter}}
+  </div>
 </template>
+<script>
+export default {
+  data() { // ページに表示するデータの指定 …（3）
+    return {
+      name: 'World', // nameの初期値 …（4）
+      counter: 42
+    }
+  }
+}
+
+</script>
